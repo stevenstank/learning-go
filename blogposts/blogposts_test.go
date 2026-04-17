2d76f71ada1dd9f7ec8af504bf4eb7db9f9cd642
stevenstank <stevenstank775@gmail.com>
2026-04-18 00:45:05 +0530

    completed io/fs

---
+++ blogposts/blogposts_test.go
@@ -0,0 +1,62 @@
+package blogposts_test
+
+import (
+	"reflect"
+	"testing"
+	"testing/fstest"
+
+	blogposts "github.com/stevenstank/learning-go/blogposts"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+		const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+		assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	})
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
