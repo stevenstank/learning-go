3fac256f2cd30199382dd3d5e71db8bb5bb87e02
stevenstank <stevenstank775@gmail.com>
2026-05-01 03:42:44 +0530

    finished go fundamentals

---
+++ templating/blogrender.go
@@ -0,0 +1,19 @@
+package blogrenderer
+
+import (
+	"io"
+	"fmt"
+)
+		
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+
+
+func Render(w io.Writer, p Post) error {
+	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
+	return err
+}
